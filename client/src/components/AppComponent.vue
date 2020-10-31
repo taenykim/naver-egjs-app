294e04afbf5cc032080ab8f342e940d56e32394d
taeeun kim <kimtaeeeny@gmail.com>
2020-10-31 21:22:11 +0900

    feat: add agent vue

--- client/src/components/AppComponent.vue
+++ client/src/components/AppComponent.vue
@@ -5,16 +5,12 @@
 	</div>
 </template>
 <script>
-import getAgent from "@egjs/agent";
 import Header from "./Header.vue";
 import ContentBox from "./ContentBox.vue";
 export default {
 	name: "AppComponent",
 	components: {Header, ContentBox},
-	mounted() {
-		const agentInfo = getAgent();
-		console.log(agentInfo);
-	},
+	mounted() {},
 };
 </script>
 <style lang="scss" scoped>
--- client/src/components/agent/AgentWrapper.vue
+++ client/src/components/agent/AgentWrapper.vue
@@ -1,9 +1,45 @@
 <template>
-	<div>agent</div>
+	<div class="wrapper">
+		<h2>Agent</h2>
+		<div class="agent-info">
+			{{ mode === "light" ? "this is light mode" : "this is dark mode" }}
+		</div>
+		<div>
+			When you use this app on your desktop, light mode is applied, and dark mode
+			is applied when you use it on mobile.
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+import getAgent from "@egjs/agent";
+
+export default {
+	data() {
+		return {
+			mode: "light",
+		};
+	},
+	mounted() {
+		const agentInfo = getAgent();
+		const isMobile = agentInfo.isMobile;
+		if (isMobile) mode = "dark";
+	},
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+	padding: 0px 20px 0px 20px;
+	background-color: #fff;
+	color: #000;
+}
+h2 {
+	margin: 0;
+	padding: 20px 0px 20px 0px;
+}
+.agent-info {
+	font-weight: bold;
+	margin-bottom: 20px;
+}
+</style>
