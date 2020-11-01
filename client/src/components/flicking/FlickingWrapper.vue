072ca367b704bead69d151f39bf01b2861a04d7e
taeeun kim <kimtaeeeny@gmail.com>
2020-11-01 16:24:56 +0900

    feat: add flicking vue

--- client/src/components/flicking/FlickingWrapper.vue
+++ client/src/components/flicking/FlickingWrapper.vue
@@ -1,11 +1,23 @@
 <template>
 	<div class="wrapper">
 		<h2>Flicking</h2>
+		<div id="flicker">
+			<div class="panel">one</div>
+			<div class="panel">two</div>
+			<div class="panel">three</div>
+			<div class="panel">four</div>
+		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import Flicking from "@egjs/flicking";
+
+export default {
+	mounted() {
+		const flicking = new Flicking("#flicker", {circular: true});
+	},
+};
 </script>
 
 <style scoped lang="scss">
@@ -15,5 +27,33 @@ export default {};
 h2 {
 	margin: 0;
 	padding: 20px 0px 20px 0px;
+}
+
+#flicker {
+	height: 300px;
+}
+.panel {
+	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 300px;
+	color: white;
+	font-size: 50px;
+	font-weight: bold;
+}
+
+.panel:nth-child(4n + 1) {
+	background-color: cornflowerblue;
+}
+.panel:nth-child(4n + 2) {
+	background-color: rgb(237, 100, 219);
+}
+.panel:nth-child(4n + 3) {
+	background-color: rgb(100, 237, 152);
+}
+.panel:nth-child(4n + 4) {
+	background-color: rgb(237, 173, 100);
 }
 </style>
