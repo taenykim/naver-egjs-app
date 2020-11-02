f11f13de89bea80eb5c4407501ec0f100b76e063
taeeun kim <kimtaeeeny@gmail.com>
2020-11-02 23:13:03 +0900

    feat: add server app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const server = express();
+const path = require("path");
+
+server.use("/", express.static(path.join(__dirname, "./dist")));
+
+server.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/dist", "index.html"));
+});
+
+server.listen(process.env.NODE_ENV === "production" ? process.env.PORT : 8080);
