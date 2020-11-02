790896887efe934a1d289f4f13767ee996f63773
taeeun kim <kimtaeeeny@gmail.com>
2020-11-02 16:37:12 +0900

    feat: add infinitegrid vue

--- client/src/components/infinitegrid/InfinitegridWrapper.vue
+++ client/src/components/infinitegrid/InfinitegridWrapper.vue
@@ -1,11 +1,46 @@
 <template>
 	<div class="wrapper">
 		<h2>Infinitegrid</h2>
+		<div id="grid"></div>
 	</div>
 </template>
 
 <script>
-export default {};
+import InfiniteGrid, {PackingLayout} from "@egjs/infinitegrid";
+import generateRandomColor from "../../utils/generateRandomColor";
+
+export default {
+	mounted() {
+		const template = () => {
+			const randomColor = generateRandomColor("0123456789ABCEF");
+			const randomWeight = Math.floor(100 + Math.random() * 100);
+			const randomHeight = Math.floor(100 + Math.random() * 100);
+			console.log({
+				randomWeight,
+				randomHeight,
+				ratio: randomWeight / randomHeight,
+			});
+			return `<div style='background-color:${randomColor}; width:${randomWeight}px; height:${randomHeight}px'></div>`;
+		};
+		const templates = Array(12)
+			.fill(0)
+			.map(_ => template());
+		const ig = new InfiniteGrid("#grid");
+
+		ig.setLayout(PackingLayout, {
+			// ratioWeight: 10000,
+			margin: 10,
+		});
+		ig.on({
+			append: function(e) {
+				const groupKey = e.groupKey + 1;
+
+				ig.append(templates, groupKey);
+			},
+		});
+		ig.append(templates, 0);
+	},
+};
 </script>
 
 <style scoped lang="scss">
@@ -15,5 +50,9 @@ export default {};
 h2 {
 	margin: 0;
 	padding: 20px 0px 20px 0px;
+}
+
+#grid {
+	height: 1200px;
 }
 </style>
